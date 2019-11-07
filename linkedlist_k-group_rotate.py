4f398492059264706129ececa478d5dd637ec219
Nitya <nityaskumar1008@gmail.com>
2019-11-07 18:57:33 -0500

    kgroup

---
+++ linkedlist_k-group_rotate.py
@@ -0,0 +1,53 @@
+# Given a linked list, reverse the nodes of a linked list k at a time and return its modified list.
+
+# k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes in the end should remain as it is.
+
+# Example:
+
+# Given this linked list: 1->2->3->4->5
+
+# For k = 2, you should return: 2->1->4->3->5
+
+# For k = 3, you should return: 3->2->1->4->5
+
+# Note:
+
+# Only constant extra memory is allowed.
+# You may not alter the values in the list's nodes, only nodes itself may be changed.
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+
+    
+    def reverseKGroup(self, head, k):
+        """
+        :type head: ListNode
+        :type k: int
+        :rtype: ListNode
+        """
+        # Dummy node initialization
+        dummy = jump = ListNode(-1)
+        dummy.next = l = r = head
+        
+        while True:
+            count = 0
+            while r and count < k:
+                count += 1
+                r = r.next
+            if count == k:
+                pre, cur = r, l
+                for _ in range(k):
+                    temp = cur.next
+                    cur.next = pre
+                    pre = cur
+                    cur = temp
+                jump.next = pre
+                jump = l
+                l = r
+            else:
+                return dummy.next
