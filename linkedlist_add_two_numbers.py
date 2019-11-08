d27abbbc4a63a8eb5741a17b054dc1ac18fa4822
Nitya <nityaskumar1008@gmail.com>
2019-11-07 21:04:14 -0500

    add ll

---
+++ linkedlist_add_two_numbers.py
@@ -0,0 +1,17 @@
+ # Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+# Explanation: 342 + 465 = 807.
+ 
+ def addTwoNumbers(self, l1, l2):
+        carry = 0;
+        res = n = ListNode(0);
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next;
+            if l2:
+                carry += l2.val;
+                l2 = l2.next;
+            carry, val = divmod(carry, 10)
+            n.next = n = ListNode(val);
+        return res.next;
